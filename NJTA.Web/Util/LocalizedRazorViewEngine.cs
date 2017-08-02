9d816d48574a1427bd82cd0af7abc8c170ede5ad
Karuppiah Ganesan <karuppiah@gmail.com>
2017-08-02 15:40:33 -0400

    RenderEngine  fix for multi language.

--- NJTA.Web/Util/LocalizedRazorViewEngine.cs
+++ NJTA.Web/Util/LocalizedRazorViewEngine.cs
@@ -1,47 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections;
+using System.Web.Mvc;
+using System.Linq;
 
 namespace NJTA.Web.Util
 {
     public class LocalizedRazorViewEngine : RazorViewEngine
     {
-        public const bool fallback = true ;
-
         public override ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
         {
             string langExtn = GetLangExtn(controllerContext);
-
-            string localizedPartialViewName = partialViewName;
-            if (!string.IsNullOrEmpty(partialViewName))
-                localizedPartialViewName += langExtn;
-
-            var result = base.FindPartialView(controllerContext, localizedPartialViewName, useCache);
-
-            if (fallback && result.View == null)
-                result = base.FindPartialView(controllerContext, partialViewName, useCache);
-
-            return result;
+            if (!string.IsNullOrEmpty(langExtn))
+            {
+                string localizedPartialViewName = string.Concat(partialViewName, langExtn);
+                ViewEngineResult result = base.FindPartialView(controllerContext, localizedPartialViewName, useCache);
+                bool viewNotExist = result.View == null && result.SearchedLocations.Any();
+                if (!viewNotExist)
+                    return result;
+            }
+            return base.FindPartialView(controllerContext, partialViewName, useCache);
         }
 
         public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
         {
             string langExtn = GetLangExtn(controllerContext);
-
-            string localizedViewName = viewName;
-            if (!string.IsNullOrEmpty(viewName))
-                localizedViewName += langExtn;
-
-            string localizedMasterName = masterName;
-            if (!string.IsNullOrEmpty(masterName))
-                localizedMasterName += langExtn;
-
-            var result = base.FindView(controllerContext, localizedViewName, localizedMasterName, useCache);
-
-            if (fallback && result.View == null)
+            if (!string.IsNullOrEmpty(langExtn))
             {
-                result = base.FindView(controllerContext, viewName, masterName, useCache);
+                string localizedViewName = string.Concat(viewName, langExtn);
+                string localizedMasterName = string.IsNullOrEmpty(masterName) ? masterName : string.Concat(masterName, langExtn);
+                ViewEngineResult result = base.FindView(controllerContext, localizedViewName, localizedMasterName, useCache);
+                bool viewNotExist = result.View == null && result.SearchedLocations.Any();
+                if (!viewNotExist)
+                    return result;
             }
-
-            return result;
+            return base.FindView(controllerContext, viewName, masterName, useCache);
         }
 
         private string GetLangExtn(ControllerContext controllerContext)
