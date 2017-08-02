6132441d9b9a91acaa8881428665f9667c91d0a2
Karuppiah Ganesan <karuppiah@gmail.com>
2017-08-01 23:30:19 -0400

    Added School Controller

---
+++ NJTA.Web/Controllers/SchoolController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace NJTA.Web.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Our Schools";
+            return View("ComingSoon");
+        }
+
+        public ActionResult Edison()
+        {
+            ViewBag.Title = "Edison";
+            return View("ComingSoon");
+        }
+
+        public ActionResult Parsippany()
+        {
+            ViewBag.Title = "Parsippany";
+            return View("ComingSoon");
+        }
+    }
+}
