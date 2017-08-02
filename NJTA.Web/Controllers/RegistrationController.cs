4428701e1b229a51519bbc028dd097d6d814628d
Karuppiah Ganesan <karuppiah@gmail.com>
2017-08-01 23:22:36 -0400

    Registration Page Updated.

---
+++ NJTA.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace NJTA.Web.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
