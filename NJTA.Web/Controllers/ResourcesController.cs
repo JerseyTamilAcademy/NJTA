8bdb25b78fe4b7d4e2e96ca69d301aa80c87ee81
Karuppiah Ganesan <karuppiah@gmail.com>
2017-09-01 17:17:49 -0400

    Resources Navigation Menu

---
+++ NJTA.Web/Controllers/ResourcesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace NJTA.Web.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        public ActionResult Guidelines()
+        {
+            ViewBag.Title = "Guidelines";
+            return View("ComingSoon");
+        }
+        public ActionResult ClassInfo()
+        {
+            ViewBag.Title = "Class Info";
+            return View("ComingSoon");
+        }
+    }
+}
