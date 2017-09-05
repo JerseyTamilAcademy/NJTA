8fa4a4e8019492222eef80d3670784db3e6cceac
Karuppiah Ganesan <karuppiah@gmail.com>
2017-09-05 01:59:55 -0400

    carouselNormalization

--- NJTA.Web/Scripts/site.js
+++ NJTA.Web/Scripts/site.js
@@ -1,4 +1,4 @@
-﻿$(window).on('resize load', function () {
+﻿$(window).on('resize load ', function () {
     $('body').css({
         "padding-top": $(".navbar").height() + "px"
     });
@@ -9,3 +9,36 @@ $(document).ready(function () {
         "padding-top": $(".navbar").height() + "px"
     });
 });
+
+function carouselNormalization() {
+    var items = $('#carousel-id .item'),
+        heights = [],
+        tallest;
+
+    // grab all the slides array, store height, find maximum and set to all items.
+    if (items.length) {
+        function normalizeHeights() {
+            items.each(function () {
+                heights.push($(this).height());
+            });                                             
+
+            tallest = Math.max.apply(null, heights);
+
+            items.each(function () {
+                $(this).css('min-height', tallest + 'px');
+            });                                             
+        };
+
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            tallest = 0, heights.length = 0;  // reset the variables
+
+            items.each(function () {
+                $(this).css('min-height', '0');  // reset each slide's height
+            });
+
+            normalizeHeights();  // run it again
+        });
+    }
+}
