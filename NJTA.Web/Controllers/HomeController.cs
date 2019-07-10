ac8101f61dfdccceaf678d898cf5642f2c8656f5
Karuppiah Ganesan <karuppiah@gmail.com>
2019-07-10 00:39:44 -0400

    Updated for 2019-20

--- NJTA.Web/Controllers/HomeController.cs
+++ NJTA.Web/Controllers/HomeController.cs
@@ -27,13 +27,10 @@ namespace NJTA.Web.Controllers
 
         public ActionResult Calendar(string id)
         {
-            return View("ComingSoon");
-            /*
             string location = FindLocation(id);
             ViewBag.Location = location;
             var calendarInfo = GetCalendarInfo(location);
             return View(calendarInfo);
-            */
         }
         
         public ActionResult Contact(string id)
