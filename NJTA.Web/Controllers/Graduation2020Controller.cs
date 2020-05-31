9f62ff8c20d39d7118c9c201bbdb24ca70921c0d
Karuppiah Ganesan <karuppiah@gmail.com>
2020-05-31 19:11:58 -0400

    Added Graduation2020

---
+++ NJTA.Web/Controllers/Graduation2020Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NJTA.Web.Controllers
+{
+    public class Graduation2020Controller : Controller
+    {
+        // GET: Graduation2020
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
