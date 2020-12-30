4434c1cd89853a1b5bcdda7b37868914a218febf
Karuppiah Ganesan <karuppiah@gmail.com>
2020-12-29 20:22:54 -0500

    Updated for 2021 Contest events

---
+++ NJTA.Web/Controllers/EventsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace NJTA.Web.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View("Contest");
+        }
+
+        public ActionResult Contest()
+        {
+            return View();
+        }
+    }
+}
