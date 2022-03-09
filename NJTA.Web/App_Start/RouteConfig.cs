33575c051016ecc2a7645f7f05c93b9db46eb8ee
Karuppiah Ganesan <karuppiah@gmail.com>
2022-03-09 18:53:02 -0500

    Fixed Routing URL format

--- NJTA.Web/App_Start/RouteConfig.cs
+++ NJTA.Web/App_Start/RouteConfig.cs
@@ -16,9 +16,9 @@ namespace NJTA.Web
                 constraints: new { lang = "en|ta" }
             );
 
-            routes.MapRoute("YouTube", "/live/{*pathInfo}", new { controller = "Redirect", action = "Index", path="https://www.youtube.com/channel/UCNTAyRT9Z3KhIFY_79l5pCQ/live" });
+            routes.MapRoute("YouTube", "live/{*pathInfo}", new { controller = "Redirect", action = "Index", path="https://www.youtube.com/channel/UCNTAyRT9Z3KhIFY_79l5pCQ/live" });
 
-            routes.MapRoute("Zoom", "/zoom/{*pathInfo}", new { controller = "Redirect", action = "Index", path="https://us02web.zoom.us/j/3879823360?pwd=WjNxc09DUGhHRTdPK054R2VvcHBQQT09" });
+            routes.MapRoute("Zoom", "zoom/{*pathInfo}", new { controller = "Redirect", action = "Index", path="https://us02web.zoom.us/j/3879823360?pwd=WjNxc09DUGhHRTdPK054R2VvcHBQQT09" });
 
             routes.MapRoute(
                 name: "Default",
