2f607bb6748aaef2a3d36bdb8d581aa5a86e9200
Karuppiah Ganesan <kganesan@powermylearning.org>
2023-08-08 21:04:10 -0400

    2023-24 Registration

--- NJTA.Web/Controllers/RegistrationController.cs
+++ NJTA.Web/Controllers/RegistrationController.cs
@@ -6,17 +6,12 @@ namespace NJTA.Web.Controllers
     {
         public ActionResult Index()
         {
+            return View("index_2023-24");
+        }
+
+        public ActionResult v23()
+        {
             return View("index_2022-23");
-        }
-
-        public ActionResult v2021()
-        {
-            return View("index_2021-22");
-        }
-
-        public ActionResult v2020()
-        {
-            return View("index_2020");
         }
     }
 }
